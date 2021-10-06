b01bb37c714005507029fb805526bfed25f82262
Joshua Perez <joshuaetperez@gmail.com>
2021-10-06 12:27:58 -0700

    Changed visual design of the header, card, and footer

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const contentDiv = document.querySelector("#content");
 const headerDiv = document.createElement("div");
 headerDiv.classList.add("header");
 const headingText = document.createElement("h3");
-headingText.innerText = "Hi There!";
+headingText.innerText = "Zenith";
 headerDiv.appendChild(headingText);
 
 // Add the image to our existing div.
@@ -20,13 +20,18 @@ headerDiv.appendChild(headingText);
 // Middle container
 const containerDiv = document.createElement("div");
 containerDiv.classList.add("container");
-containerDiv.textContent = "This is the container.";
+// containerDiv.textContent = "This is the container.";
+
+const cardDiv = document.createElement("div");
+cardDiv.classList.add("card");
+cardDiv.textContent = "Welcome to Zenith, a towering restaurant floating above the heavens! Eat at one of the world's finest establishments while taking a look at the most beautiful sights from above!";
+containerDiv.appendChild(cardDiv);
 
 
 // Footer
 const footerDiv = document.createElement("div");
 footerDiv.classList.add("footer");
-footerDiv.textContent = "This is the footer.";
+footerDiv.textContent = "Made with the approval of the Gods";
 
 contentDiv.appendChild(headerDiv);
 contentDiv.appendChild(containerDiv);
