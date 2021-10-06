b1fa0addb5d661e29add4897eeeccea9709c24b3
Joshua Perez <joshuaetperez@gmail.com>
2021-10-06 14:29:06 -0700

    Added a navbar and made other visual changes

--- src/home.js
+++ src/home.js
@@ -10,6 +10,28 @@ export default function displayHome() {
     headingText.innerText = "Zenith";
     headerDiv.appendChild(headingText);
 
+    // Navbar
+    const navDiv = document.createElement("div");
+    navDiv.classList.add("navbar");
+    const tabList = document.createElement("ul");
+    const tabHome = document.createElement("li");
+    const tabMenu = document.createElement("li");
+    const tabContact = document.createElement("li");
+    const homeButton = document.createElement("span");
+    const menuButton = document.createElement("span");
+    const contactButton = document.createElement("span");
+    tabHome.classList.add("selected-tab");
+    homeButton.textContent = "Home";
+    menuButton.textContent = "Menu";
+    contactButton.textContent = "Contact";
+    tabHome.appendChild(homeButton);
+    tabMenu.appendChild(menuButton);
+    tabContact.appendChild(contactButton);
+    tabList.appendChild(tabHome);
+    tabList.appendChild(tabMenu);
+    tabList.appendChild(tabContact);
+    navDiv.appendChild(tabList);
+
     // Middle container
     const containerDiv = document.createElement("div");
     containerDiv.classList.add("container");
@@ -24,9 +46,14 @@ export default function displayHome() {
     const footerDiv = document.createElement("div");
     footerDiv.classList.add("footer");
     footerDiv.textContent = "Made with the approval of the Gods";
+    const disclaimerDiv = document.createElement("div");
+    disclaimerDiv.classList.add("disclaimer");
+    disclaimerDiv.textContent = "*Zenith does not take responsibility for any painful injuries or deaths resulting from falling from a high place*";
+    footerDiv.appendChild(disclaimerDiv);
 
     // Append to contentDiv
     contentDiv.appendChild(headerDiv);
+    contentDiv.appendChild(navDiv);
     contentDiv.appendChild(containerDiv);
     contentDiv.appendChild(footerDiv);
 }
